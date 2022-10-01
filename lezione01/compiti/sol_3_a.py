48724b491d9cbdf4f34cd4942274daf88911f6d9
PancakeR9 <114255339+PancakeR9@users.noreply.github.com>
2022-10-01 18:34:19 +0200

    Esercizio 3 lezione 1

--- lezione01/compiti/sol_3_a.py
+++ lezione01/compiti/sol_3_a.py
@@ -1,24 +1,39 @@
+n1 = int(input("Salve giovane passante, metti alla prova i miei numeri. Dimmene uno "))
+n2 = int(input("Gran bel numero. Perché la mia magia sia valida me ne serve un altro però "))
+sum = n1 + n2
+print("La somma di " + str(n1) + " e " + str(n2) + " è " + str(sum))
+n3 = int(input("Guarda che altra magia so fare; dimmi un altro numero ancora "))
+n4 = int(input("Ancora uno dai "))
+dif = n3 - n4
+print("La differenza di " + str(n3) + " e " +str(n4) + " è "+ str(dif))
+average = ( n1 + n2 + n3 + n4 )/ 4
+print("E adesso osserva. La media di " + str(n1) + " "+ str(n2) + " " + str(n3) + " " + str(n4) + " è " + str(average))
+if n1>n2 and n1>n3 and n1>n4:
+  print("Il numero massimo è "  + str(n1))
+if n2>n1 and n2>n3 and n2>n4:
+  print("Il numero massimo è " + str(n2))
+if n3>n1 and n3>n2 and n3>n4:
+  print("Il numero massimo è " + str(n3))
+if n4>n1 and n4>n2 and n4>n3:
+  print("Il numero massimo è " + str(n4))
+if n1<n2 and n1<n3 and n1<n4:
+  print("Il numero minimo è " + str(n1))
+if n2<n1 and n2<n3 and n2<n4:
+  print("Il numero minimo è " + str(n2))
+if n3<n1 and n3<n2 and n3<n4:
+  print("Il numero minimo è " + str(n3))
+if n4<n1 and n4<n2 and n4<n3:
+  print("Il numero minimo è " + str(n4))
 
+# soluzione alternativa di Claudio
+max = n1
+if n2 > max:
+  max = n2
 
-n1 = input("Metti alla prova i miei numeri. Dimmene uno")
+if n3 > max:
+  max = n3
 
+if n4 > max:
+  max = n4
 
-if len(n1)>0:
-
-    print("Gran bel numero")
-
-    n2 = input("Scegline ancora un altro giovane passante")
-
-
-    sum = int(n1) + int(n2)
-
-
-    if len(n2)>0:
-        print(sum)
-        print(dif)
-    else:
-        print("non hai ancora scelto un secondo numero")
-else:
-    print("Come pensi di mettermi alla prova se non mi dai un numero?")
- 
-
+print(max)
